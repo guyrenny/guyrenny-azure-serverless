af55c7ff9114c78c5090b35004a721f2299a0e1f
Chen Harel <chook.harel@gmail.com>
2021-10-06 10:12:25 -0500

    protecting against null and missing arrays (#7)

--- EventHub/EventHub/index.ts
+++ EventHub/EventHub/index.ts
@@ -6,7 +6,7 @@
  * @link        https://coralogix.com/
  * @copyright   Coralogix Ltd.
  * @licence     Apache-2.0
- * @version     1.0.0
+ * @version     1.1.0
  * @since       1.0.0
  */
 
@@ -19,9 +19,9 @@ import { Log, Severity, CoralogixLogger, LoggerConfig } from "coralogix-logger";
  * @param {array} eventHubMessages - event hub messages
  */
 const eventHubTrigger: AzureFunction = function (context: Context, events: any): void {
-    context.log(`eventHub trigger function named: ${context.executionContext.functionName}`);
+    context.log(`eventHub trigger function named: ${context.executionContext.functionName} version 1.1.0`);
 
-    if (events.length === 0) {
+    if ((!Array.isArray(events)) || (events.length === 0)) {
         return;
       }
 
@@ -32,21 +32,41 @@ const eventHubTrigger: AzureFunction = function (context: Context, events: any):
     }));
 
     const logger: CoralogixLogger = new CoralogixLogger("eventhub");
+    const threadId: string = context.executionContext.functionName;
 
     events.forEach((record) => {
-        record.records.forEach((inner) => {
-            const body = JSON.stringify(inner);
+        if (record == null) {
+            return;
+        }
 
-            logger.addLog(new Log({
-                severity: Severity.info,
-                text: body,
-                threadId: context.executionContext.functionName
-            }));
-        });
+        if (Array.isArray(record)) {
+            record.forEach((inner) => {
+                writeLog(inner, threadId, logger);
+            });
+        }
+        else if (Array.isArray(record.records)) {
+            record.records.forEach((inner) => {
+                writeLog(inner, threadId, logger);
+            });
+        }
     });
 
     CoralogixLogger.flush();
     context.done();
+};
+
+const writeLog = function(text: any, thread: any, logger: CoralogixLogger): void {
+    if (text == null) {
+        return;
+    }
+
+    const body = JSON.stringify(text);
+
+    logger.addLog(new Log({
+        severity: Severity.info,
+        text: body,
+        threadId: text
+    }));
 };
 
 export default eventHubTrigger;
